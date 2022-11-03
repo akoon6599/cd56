c7e638d6dabaa846f9da56c02f73015a0f4dd43e
aila mengden-koon <akoon6599@student.pps.net>
2022-11-03 09:50:23 -0700

    start passGen assignment

---
+++ src/passGen/passGen.java
@@ -0,0 +1,9 @@
+package passGen;
+
+public class passGen {
+    static final String CHARACTER_POOL = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~";
+
+    public static void main(String[] args) {
+
+    }
+}
