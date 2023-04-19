2b2dae2fa71db6d00e43e120036e336e1d17004a
aila mengden-koon <akoon6599@student.pps.net>
2023-04-19 14:19:13 -0700

    modularity

--- src/mazeRecursion/Interface.java
+++ src/mazeRecursion/Interface.java
@@ -19,34 +19,63 @@ public class Interface {
     public static void main(String[] args) {
         Map mainMap = new Map(
                 """
-                m........c
+                ..........
                 .####.####
-                ......####
+                ..c...####
                 #####.####
                 ####..####
-                ####..####
-                .###.#####
-                c....#####
-                #c#.######
+                ####m.####
+                c###.#####
+                .....#####
+                #.#.######
                 ###c######
                 """
         );
 
-        solve(mainMap);
+        solve(1, mainMap);
     }
 
-    private static void solve(Map mainMap) {
-        // Edit here to change which cheese to find
-        int num = 3;
-
+    // Solve Map for specific solution
+    private static void solve(int num, Map mainMap) {
+        // Format map displays
         System.out.println("       # Character Map #");
         mainMap.print(false);
         System.out.println();
         System.out.printf("     # Solving:  Answer %d #%n", num);
         System.out.println("       #  Weights Map  #");
+
+        // Calculate weights for each cell
         mainMap.assignWeights(0, mainMap.endPositions.get(num));
         mainMap.print(true);
 
+        // Find the optimal path
+        HashMap<Integer, Integer[]> pathHistory = new HashMap<>();
+        pathHistory.put(0, mainMap.startPosition);
+        pathHistory = mainMap.solveMap(0, pathHistory);
+        mainMap.map.get(mainMap.startPosition[0]).get(mainMap.startPosition[1]).idealPath = true;
+
+        System.out.println();
+        System.out.println("       #   Solved Map  #");
+        mainMap.print(false);
+        System.out.printf("     # %d Steps to Solve #", pathHistory.size()-1);
+    }
+
+    // Solve general shortest path
+    private static void solve(Map mainMap) {
+        // Format map displays
+        System.out.println("       # Character Map #");
+        mainMap.print(false);
+        System.out.println();
+        System.out.println("   # Solving General Solution #");
+        System.out.println("       #  Weights Map  #");
+
+        // Calculate weights for each cell
+        for (Integer[] pos : mainMap.endPositions.values()) {
+            mainMap.assignWeights(0, pos);
+        }
+        mainMap.print(true);
+
+        // Find the optimal path
         HashMap<Integer, Integer[]> pathHistory = new HashMap<>();
         pathHistory.put(0, mainMap.startPosition);
         pathHistory = mainMap.solveMap(0, pathHistory);
@@ -76,6 +105,7 @@ class Map {
                             .collect(Collectors.toCollection(ArrayList::new))
             );
         }
+        // Get position pairs for each possible ending
         int endCount = 0;
         for (int i = 0; i<map.size(); i++) {
             for (int j = 0; j<map.get(i).size(); j++) {
@@ -91,6 +121,7 @@ class Map {
         }
     }
 
+    // Single method to print weight map and char map
     public void print(boolean weights) {
         for (List<MapCell> row: map) {
             for (MapCell cell: row) {
@@ -101,15 +132,19 @@ class Map {
         }
     }
 
+    // Calculate weights of each cell based off distance from selected ending
     public void assignWeights(int prevWeight, Integer[] startPosition) {
         ArrayList<MapCell> nearbyCells = getNearbyCells(startPosition);
 
         for (MapCell cell : nearbyCells) {
+            // Do nothing for walls or cells with an already lower weight
             if (cell.type != MapCell.CellType.WALL && (Objects.equals(cell.weight, "x") || Integer.parseInt(cell.weight) > prevWeight) && prevWeight <= 50) {
-               cell.weight = Integer.toString(prevWeight+1);
+                cell.weight = Integer.toString(prevWeight+1);
+                // Set low value for the desired ending to force solver to choose it
                 if (cell.type == MapCell.CellType.CHEESE && (Objects.equals(cell.position[1], startPosition[1]) && Objects.equals(cell.position[0], startPosition[0]))) {
                     cell.weight = Integer.toString(-1);
                 }
+                // Recursively follow path with new cell as start position
                 if (cell.position != startPosition) {
                     assignWeights(prevWeight + 1, cell.position);
                 }
@@ -117,9 +152,11 @@ class Map {
         }
     }
 
+    // Follow the lowest weight values to ending
     public HashMap<Integer, Integer[]> solveMap(int depth, HashMap<Integer, Integer[]> pathHistory) {
         ArrayList<MapCell> nearbyCells = getNearbyCells(pathHistory.get(depth));
 
+        // Find the cell with the lowest weight value
         int lowestAt = 0;
         for (int i=0; i<nearbyCells.size(); i++) {
             if (!nearbyCells.get(i).weight.equals("x")) {
@@ -134,6 +171,7 @@ class Map {
         pathHistory.put(depth, nearbyCells.get(lowestAt).position);
         nearbyCells.get(lowestAt).idealPath = true;
 
+        // If at ending return the history Map, else recursively solve for the next cell
         if (nearbyCells.get(lowestAt).type == MapCell.CellType.CHEESE) {
             return pathHistory;
         }
@@ -142,6 +180,7 @@ class Map {
         }
     }
 
+    // Return a list of all adjacent cells
     private ArrayList<MapCell> getNearbyCells(Integer[] position) {
         ArrayList<MapCell> nearbyCells = new ArrayList<>();
         for (int i=-1;i<=1;i++) {
@@ -174,12 +213,16 @@ class MapCell {
             case "#" -> type = CellType.WALL;
         }
     }
+
+    // Print special symbols for taken path
     public void print() {
         if (idealPath && type == CellType.START) {System.out.print("  +");}
         else if (idealPath && type == CellType.CHEESE) {System.out.print("  *");}
         else if (idealPath) {System.out.print("  O");}
         else {System.out.printf("  %s",repr);}
     }
+
+    // Print weight values in a uniform spacing
     public void printWeightMap() {
         System.out.printf(" %s", weight.length()==1 ? " "+weight : weight);
     }
